b075c7056df9719127dc248a795dcadbf60d85d7
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-05-17 14:29:23 -0600

    basic routes prepared for improvement

--- server/server.js
+++ server/server.js
@@ -1,17 +1,49 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-app.get("/getRestaurants", (req, res) => {
-	res.json({
+app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log("hitting middleware");
+	next();
+})
+
+// Get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+	res.status(200).json({
 		status: "success",
-		restaurant: "mcdonalds",
-	}
-	);
+		data: {
+			restaurant: ["mcdonals", "wendys"],
+		}
+	});
+});
+
+// get a restaurant
+app.get("/api/v1/restaurants/:restaurantid", (req, res) => {
+	console.log(req.params);	
+});
+
+// Create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+	console.log(req);
+});
+
+// update restaurants
+app.put("/api/v1/restaurants/:id", (req, res) => {
+	console.log(req.params.id);
+	console.log(req.body);
+});
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+	res.status(204).json({
+		status: "success",
+	});
 });
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-	console.log("server is up and listening on port 3006");
+	console.log("server is up and listening on port 3001");
 });
