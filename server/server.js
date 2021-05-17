64e805abd293a0bef29118a70762518cff59f3dd
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-05-17 17:37:09 -0600

    expanded on the route for creating a restaurant

--- server/server.js
+++ server/server.js
@@ -45,8 +45,21 @@ app.get("/api/v1/restaurants/:id", async (req, res) => {
 });
 
 // Create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
+app.post("/api/v1/restaurants", async (req, res) => {
 	console.log(req);
+	
+	try {
+		const results = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *", [req.body.name, req.body.location, req.body.price_range]);
+		console.log(results);
+		res.status(201).json({
+			status: "success",
+			data: {
+				restaurant: results.rows[0],
+			},
+		});
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 // update restaurants
