68f3e66cb6bd0b6d334dd4e34ae5e2e3b06b3ad5
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-05-18 17:56:22 -0600

    added star rating component

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
+import StarRating from "../components/StarRating";
 
 const RestaurantDetailPage = () => {
 	const {id} = useParams();
-	const {selectedRestaurant, setSelectedRestaurant} = useContext(null);
+	const {selectedRestaurant, setSelectedRestaurant} = useContext(RestaurantsContext);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -22,7 +23,7 @@ const RestaurantDetailPage = () => {
 	}, []);
 	return (
 		<div>
-			{selectedRestaurant && selectedRestaurant.name}
+			{selectedRestaurant && <StarRating rating={3.5}/>}
 		</div>
 	)
 }
